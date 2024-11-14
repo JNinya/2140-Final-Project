b2fca00eb331348a6c1e65317606702491f5904f
JNinya <jninya16@gmail.com>
2024-11-14 13:35:17 -0500

    simplified symmetric encryption

--- crypto.py
+++ crypto.py
@@ -9,11 +9,13 @@ Token - the encrypted version of the message
 
 
 """
-#This is symmetric encryption using the fernet algorithm
+"""#This is symmetric encryption using the fernet algorithm
 class Crypto:
+
     #you can set the data of the Crypto object when you instantiate the class if you want
     def __init__(self, data = ""):
         self.data = data.encode('ascii')
+        self.key = Crypto.generateFernetKey()
 
     #reads a file and sets self.data to the content of the file
     def readFile(self, file_name):
@@ -41,3 +43,37 @@ class Crypto:
     def decrypt(self):
         f = Fernet(self.key)
         self.data = f.decrypt(self.token)
+
+
+
+class sym:
+        #converts data to token using set key
+        def encrypt(self):
+            f = Fernet(self.key.encode('ascii'))
+            self.token = f.encrypt(self.data.encode('ascii')).decode('ascii')
+        
+        #converts token to data using set key
+        def decrypt(self):
+            f = Fernet(self.key)
+            self.data = f.decrypt(self.token)"""
+
+
+
+def generateSymKey():
+    key = Fernet.generate_key()
+    return key.decode('ascii')
+
+def sym_encrypt(data, key):
+    data = data.encode('ascii')
+    key = key.encode('ascii')
+
+    f = Fernet(key)
+    return f.encrypt(data).decode('ascii')
+
+def sym_decrypt(token, key):
+    token = token.encode('ascii')
+    key = key.encode('ascii')
+
+
+    f = Fernet(key)
+    return f.decrypt(token).decode('ascii')
--- main.py
+++ main.py
@@ -11,43 +11,18 @@ def start():
 
 
 def encrypt():
-    #creates a new Crypto object
-    text = Crypto()
-
-    #asks user to input text and sets the data to that
-    text.data = input("Enter text to encrypt: ").encode('ascii')
-
-    #generates a new random fernet key
-    key = Crypto.generateFernetKey()
-
-    #tells you what the key is
-    print("Key:", key.decode('ascii'))
-
-    #sets the key used for encryption to the one generated
-    text.setKey(key)
-
-    #encrypts data to token
-    text.encrypt()
-
-    #print encrypted text (token)
-    print("Encrypted text:", text.token.decode('ascii'))
+    
+    data = input("Enter text to encrypt: ")
+    key = generateSymKey()
+    token = sym_encrypt(data, key)
+    print("Key:", key)
+    print("Encrypted text:", token)
 
 def decrypt():
     token = input("Enter encrypted text: ")
     key = input("Enter key: ")
+    data = sym_decrypt(token, key)
 
-    text = Crypto()
-    text.setToken(token)
-    text.setKey(key)
-    text.decrypt()
-    print(text.data.decode('ascii'))
-
+    print(data)
 
 start()
-
-"""
-example token to decrypt
-Key:  b'ingHz_VHuIfpXdhMFpgIb9LRe0x5euQ1SKLstKr0_SM='
-b'gAAAAABnNjTPAYJtMsPvelcreQvk9Tzy3TQBz7EQtcwxWH7xGfqKz7CCeQF_RyF2RqT1qZfM1rQYoWt_lNWBHYQ4t9hnTmkGZ1fQgGaoyL5JSwXQPDRnNwfWgJxwA-UHZJO2j8YOaJJkHkFianae4ixL_myMgFMOv_N2fmW9cZDnMvYffvUGsnmug7-6rXMjUqIl9g4bX0Ka'
-
-"""

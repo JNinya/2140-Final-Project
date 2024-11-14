0a3f307719b994257ba966512c77aeed9953e3d3
JNinya <jninya16@gmail.com>
2024-11-14 12:45:49 -0500

    shitty but working symmetric encryption

--- crypto.py
+++ crypto.py
@@ -1,20 +1,43 @@
 from cryptography.fernet import Fernet
 
-class Crypto:
-    def __init__(self, data = b""):
-        self.data = data.encode('ascii')
-        self.key = b""
+"""
+The Crypto class contains 3 main pieces of info:
 
-    def openFile(self, file_name):
-        with open(file_name, "r", encoding="utf8") as file:
-            self.data = file.read()
+Data - the raw unencrypted text of a message
+Key - the key used for encryption of a message
+Token - the encrypted version of the message
+
+
+"""
+#This is symmetric encryption using the fernet algorithm
+class Crypto:
+    #you can set the data of the Crypto object when you instantiate the class if you want
+    def __init__(self, data = ""):
+        self.data = data.encode('ascii')
+
+    #reads a file and sets self.data to the content of the file
+    def readFile(self, file_name):
+        with open(file_name, "r", encoding="ascii") as file:
+            self.data = file.read().encode('ascii')
     
+    #generates a 32-byte fernet key for symmetric encryption
+    def generateFernetKey():
+        key = Fernet.generate_key()
+        return key
+    
+    #sets self.key to the key you input
     def setKey(self, key):
         self.key = key
 
+    def setToken(self, token):
+        self.token = token
+
+    #converts data to token using set key
     def encrypt(self):
-        key = Fernet.generate_key()
-        print(key)
-        f = Fernet(key)
-        token = f.encrypt(self.data)
-        return token
+        f = Fernet(self.key)
+        self.token = f.encrypt(self.data)
+    
+    #converts token to data using set key
+    def decrypt(self):
+        f = Fernet(self.key)
+        self.data = f.decrypt(self.token)
--- main.py
+++ main.py
@@ -1,8 +1,53 @@
 from crypto import *
 
-text = input("Enter text to encrypt: ")
+def start():
+    choice = input("Would you like to encrypt or decrypt (e/d)?")
+    if choice == "e":
+        encrypt()
+    elif choice == "d":
+        decrypt()
+    else:
+        start()
 
-file = Crypto(text)
-#file.setKey(b"babaganooshbabaganooshbabaganoos")
-print(file.data)
-print("Token = ", file.encrypt())
+
+def encrypt():
+    #creates a new Crypto object
+    text = Crypto()
+
+    #asks user to input text and sets the data to that
+    text.data = input("Enter text to encrypt: ").encode('ascii')
+
+    #generates a new random fernet key
+    key = Crypto.generateFernetKey()
+
+    #tells you what the key is
+    print("Key:", key.decode('ascii'))
+
+    #sets the key used for encryption to the one generated
+    text.setKey(key)
+
+    #encrypts data to token
+    text.encrypt()
+
+    #print encrypted text (token)
+    print("Encrypted text:", text.token.decode('ascii'))
+
+def decrypt():
+    token = input("Enter encrypted text: ")
+    key = input("Enter key: ")
+
+    text = Crypto()
+    text.setToken(token)
+    text.setKey(key)
+    text.decrypt()
+    print(text.data.decode('ascii'))
+
+
+start()
+
+"""
+example token to decrypt
+Key:  b'ingHz_VHuIfpXdhMFpgIb9LRe0x5euQ1SKLstKr0_SM='
+b'gAAAAABnNjTPAYJtMsPvelcreQvk9Tzy3TQBz7EQtcwxWH7xGfqKz7CCeQF_RyF2RqT1qZfM1rQYoWt_lNWBHYQ4t9hnTmkGZ1fQgGaoyL5JSwXQPDRnNwfWgJxwA-UHZJO2j8YOaJJkHkFianae4ixL_myMgFMOv_N2fmW9cZDnMvYffvUGsnmug7-6rXMjUqIl9g4bX0Ka'
+
+"""
